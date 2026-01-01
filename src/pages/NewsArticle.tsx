fe90cd5f06ca2dfb8714f92ede5e173ca30b7354
praveen-kumar-007 <praveen.pr105@gmail.com>
2026-01-01 20:08:13 +0530

    fix: remove unused React import in NewsArticle

--- src/pages/NewsArticle.tsx
+++ src/pages/NewsArticle.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { Calendar, Tag, ArrowLeft } from 'lucide-react';
 
